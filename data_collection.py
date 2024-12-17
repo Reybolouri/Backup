5a0224a6973944cb685bada9339df0a246662217
RBolourui <160203017+RBolourui@users.noreply.github.com>
2024-12-17 11:24:10 -0600

    Updated

--- data_collection.py
+++ data_collection.py
@@ -83,7 +83,6 @@ def update_bls_data():
     combined_df.to_csv(CSV_file, index=False)
     print(f"Data successfully updated and saved to {CSV_file}.")
     
-
 # Main execution
 if __name__ == "__main__":
     update_bls_data()
